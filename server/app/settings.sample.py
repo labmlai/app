10eac8d873f92d4e267e3d4a3f718519679ccb35
hnipun <hnipun@gmail.com>
2020-08-25 07:01:48 +0530

    modified readme

--- server/app/settings.sample.py
+++ server/app/settings.sample.py
@@ -2,6 +2,6 @@ from pathlib import PurePath
 
 SLACK_CLIENT_ID = 'XXX'
 SLACK_CLIENT_SECRET = 'XXX'
-SERVER_URL = 'https://api.lab-ml.com'
+SERVER_URL = 'http://api.lab-ml.com'
 WEB_URL = 'https://web.lab-ml.com'
 DATA_PATH = PurePath('/xxx/xxx/data')
