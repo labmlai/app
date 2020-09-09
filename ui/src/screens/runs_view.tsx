10c463673c2d9b27b883632bda17916393bf7b0d
hnipun <hnipun@gmail.com>
2020-09-09 10:55:08 +0530

    loader for runs_view

--- ui/src/screens/runs_view.tsx
+++ ui/src/screens/runs_view.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react"
+import Loader from 'react-loader-spinner'
+
 
 import NETWORK from '../network'
 import {RunsTable} from "../components/runs_table"
 import {Markdown} from "../components/markdown"
-import {Run} from "../components/utils"
 
 
 interface RunsProps {
@@ -11,7 +12,8 @@ interface RunsProps {
 }
 
 function RunsView(props: RunsProps) {
-    const [runs, setRuns] = useState<Run[]>([]);
+    const [isLoading, setIsLoading] = useState(true)
+    const [runs, setRuns] = useState<any[]>([])
 
     const params = new URLSearchParams(props.location.search)
     const labMlToken = params.get('labml_token')
@@ -20,17 +22,37 @@ function RunsView(props: RunsProps) {
     useEffect(() => {
         if (labMlToken) {
             NETWORK.get_runs(labMlToken).then((res) => {
-                setRuns(res.data)
+                if (res) {
+                    setRuns(res.data)
+                    setIsLoading(false)
+                }
             })
         }
     }, [labMlToken])
 
-    return <div className={'pt-5'}>
-        {runs.length !== 0
-            ? <RunsTable runs={runs}/>
-            :
-            <Markdown labMlToken={labMlToken}/>
-        }
+    return <div>
+        {(() => {
+            if (isLoading) {
+                return (
+                    <div className={'loader'}>
+                        <Loader
+                            type="Bars"
+                            color="#00BFFF"
+                            height={100}
+                            width={100}
+                        />
+                    </div>
+                )
+            } else if (runs.length === 0) {
+                return (
+                    <Markdown labMlToken={labMlToken}/>
+                )
+            } else {
+                return (
+                    <RunsTable runs={runs}/>
+                )
+            }
+        })()}
     </div>
 }
 
