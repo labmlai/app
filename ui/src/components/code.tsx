0e1cfe17d71c07d022437d23e442d742da9b86ba
hnipun <hnipun@gmail.com>
2020-09-09 15:12:17 +0530

    style

--- ui/src/components/code.tsx
+++ ui/src/components/code.tsx
@@ -9,25 +9,25 @@ interface MarkdownProps {
 
 export function Code(props: MarkdownProps) {
     const code =
-        `
-        import numpy as np
-        from labml import tracker, experiment
-        
-        configs = {
-            'fs': 100000,  # sample rate
-            'f': 1,  # the frequency of the signal
-        }
-        
-        x = np.arange(configs['fs'])
-        y = np.sin(2 * np.pi * configs['f'] * (x / configs['fs']))
-        
-        experiment.record(name='sin_wave', conf_dict=configs, lab_conf={'web_api':
-                         'https://api.lab-ml.com/api/v1/track?labml_token=${props.labMlToken}'})
-        with experiment.start():
-            for y_i in y:
-                tracker.save({'loss': y_i, 'noisy': y_i + np.random.normal(0, 10, 100)})
-                tracker.add_global_step() 
-     `
+    `    
+    import numpy as np
+    from labml import tracker, experiment
+    
+    configs = {
+        'fs': 100000,  # sample rate
+        'f': 1,  # the frequency of the signal
+    }
+    
+    x = np.arange(configs['fs'])
+    y = np.sin(2 * np.pi * configs['f'] * (x / configs['fs']))
+    
+    experiment.record(name='sin_wave', conf_dict=configs, lab_conf={'web_api':
+                     'https://api.lab-ml.com/api/v1/track?labml_token=${props.labMlToken}'})
+    with experiment.start():
+        for y_i in y:
+            tracker.save({'loss': y_i, 'noisy': y_i + np.random.normal(0, 10, 100)})
+            tracker.add_global_step() 
+    `
 
     return <div>
         <LabLoader isLoading={true}/>
