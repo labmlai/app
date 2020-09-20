3e6288e5086163b2b9b1597f8ffb61fb931f32aa
hnipun <hnipun@gmail.com>
2020-09-20 16:09:58 +0530

    changed lottie json

--- ui/src/screens/page_not_found_view.tsx
+++ ui/src/screens/page_not_found_view.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Lottie from 'lottie-react-web'
-import animation from '../assets/lottie/astronaout.json'
+import animation from '../assets/lottie/Chemical.json'
 
 
 function PageNotFound() {
     return <div className={'container text-center mt-5'}>
         <h1 className={'display-1 text-danger'}>404</h1>
-        <h1 className={'text-primary'}>YOU ARE IN THE WRONG SPACE</h1>
+        <h1 className={'text-primary'}>ERROR - LAB EXPLOSION</h1>
         <Lottie style={{width: 500, margin: '0 auto'}}
                 options={{
                     animationData: animation
