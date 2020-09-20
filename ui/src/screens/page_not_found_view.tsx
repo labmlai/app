901dcbdd449adf96d2371670c041d4bd477e84dc
hnipun <hnipun@gmail.com>
2020-09-20 14:00:27 +0530

    page not found ui modifications

--- ui/src/screens/page_not_found_view.tsx
+++ ui/src/screens/page_not_found_view.tsx
@@ -5,8 +5,9 @@ import animation from '../assets/lottie/astronaout.json'
 
 function PageNotFound() {
     return <div className={'container text-center mt-5'}>
-        <h1 className={'text-secondary'}>SORRY, WE COULDN'T FIND THE SPACE</h1>
-        <Lottie style={{width: 400, margin: '0 auto'}}
+        <h1 className={'display-1 text-danger'}>404</h1>
+        <h1 className={'text-primary'}>YOU ARE IN THE WRONG SPACE</h1>
+        <Lottie style={{width: 500, margin: '0 auto'}}
                 options={{
                     animationData: animation
                 }}
