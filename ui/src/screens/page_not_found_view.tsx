97a7e3e74bb1ef21d7f5a7cea9da1d33157377c4
hnipun <hnipun@gmail.com>
2020-09-20 20:40:17 +0530

    404 error page style modifications

--- ui/src/screens/page_not_found_view.tsx
+++ ui/src/screens/page_not_found_view.tsx
@@ -4,10 +4,12 @@ import animation from '../assets/lottie/Chemical.json'
 
 
 function PageNotFound() {
-    return <div className={'container text-center mt-5'}>
-        <h1 className={'display-1 text-danger'}>404</h1>
-        <h1 className={'text-primary'}>ERROR - LAB EXPLOSION</h1>
-        <Lottie style={{width: 500, margin: '0 auto'}}
+    return <div className={'container'}>
+        <div className={'mt-5 text-center'}>
+            <h1 className={'display-1 text-danger'}>404</h1>
+            <h1 className={'text-primary'}>ERROR - LAB EXPLOSION</h1>
+        </div>
+        <Lottie style={{width: 500}}
                 options={{
                     animationData: animation
                 }}
