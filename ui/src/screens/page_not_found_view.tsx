b8257b7b10cdb275010e69373b20e4a439b264c4
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-09-20 20:43:33 +0530

    lottie size

--- ui/src/screens/page_not_found_view.tsx
+++ ui/src/screens/page_not_found_view.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Lottie from 'lottie-react-web'
 import animation from '../assets/lottie/Chemical.json'
+import useWindowDimensions from "../utils/window_dimensions";
 
 
 function PageNotFound() {
+    const {width: windowWidth} = useWindowDimensions()
+
     return <div className={'container'}>
         <div className={'text-center mt-5'}>
             <h1 className={'display-1 text-danger'}>404</h1>
             <h1 className={'text-primary'}>ERROR - LAB EXPLOSION</h1>
-            <Lottie style={{width: 500, margin: '0 auto'}}
+            <Lottie style={{width: Math.min(windowWidth, 500), margin: '0 auto'}}
                     options={{
                         animationData: animation
                     }}
