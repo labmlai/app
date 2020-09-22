a7695953c23895a1e69ef3a1707e2c19f01f69c6
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-09-22 08:35:59 +0530

    ✨ multiple cache requests

--- ui/src/cache/cache.tsx
+++ ui/src/cache/cache.tsx
@@ -3,12 +3,68 @@ import {Run, RunModel, SeriesModel} from "../models/run";
 
 const TRACKING_TIMEOUT = 60 * 1000
 
+class BroadcastPromise<T> {
+    private isLoading: boolean;
+    private resolvers: any[]
+    private rejectors: any[]
+
+    constructor() {
+        this.isLoading = false
+        this.resolvers = []
+        this.rejectors = []
+    }
+
+    private add(resolve: (value: T) => void, reject: (err: any) => void) {
+        this.resolvers.push(resolve)
+        this.rejectors.push(reject)
+    }
+
+    create(load: () => Promise<T>): Promise<T> {
+        let promise = new Promise<T>((resolve, reject) => {
+            this.add(resolve, reject)
+        })
+
+        if (!this.isLoading) {
+            load().then((res) => {
+                this.resolve(res)
+            }).catch((err) => {
+                this.reject(err)
+            })
+        }
+
+        return promise
+    }
+
+    private resolve(value: T) {
+        this.isLoading = false
+        let resolvers = this.resolvers
+        this.resolvers = []
+        this.rejectors = []
+
+        for (let r of resolvers) {
+            r(value)
+        }
+    }
+
+    private reject(err: any) {
+        this.isLoading = false
+        let rejectors = this.rejectors
+        this.resolvers = []
+        this.rejectors = []
+
+        for (let r of rejectors) {
+            r(err)
+        }
+    }
+}
 
 class RunCache {
     private uuid: string
     private lastUpdated: number
     private run?: Run
     private tracking?: SeriesModel[]
+    private runPromise = new BroadcastPromise<RunModel>()
+    private trackingPromise = new BroadcastPromise<SeriesModel[]>()
 
     constructor(uuid: string) {
         this.uuid = uuid
@@ -16,33 +72,24 @@ class RunCache {
     }
 
     private async loadRun(): Promise<RunModel> {
-        return new Promise<RunModel>(((resolve, reject) => {
-            NETWORK.get_run(this.uuid)
-                .then((res) => {
-                    resolve(res.data)
-                })
-                .catch((err) => {
-                    reject(err.message)
-                })
-        }))
+        return this.runPromise.create(async () => {
+            let res = await NETWORK.get_run(this.uuid)
+            return res.data
+        })
     }
 
     private async loadTracking(): Promise<SeriesModel[]> {
-        return new Promise<SeriesModel[]>(((resolve, reject) => {
-            NETWORK.get_tracking(this.uuid)
-                .then((res) => {
-                    resolve(res.data)
-                })
-                .catch((err) => {
-                    reject(err.message)
-                })
-        }))
+        return this.trackingPromise.create(async () => {
+            let res = await NETWORK.get_tracking(this.uuid)
+            return res.data
+        })
     }
 
     async getRun(): Promise<Run> {
         if (this.run == null) {
             this.run = new Run(await this.loadRun())
         }
+        console.log(this.run)
         return this.run
     }
 
