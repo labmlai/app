be88dbe01540ae9e0073147efedbad5c95a59193
hnipun <hnipun@gmail.com>
2020-09-27 19:03:43 +0530

    loader when google sign-in button clicked

--- ui/src/screens/login_view.tsx
+++ ui/src/screens/login_view.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {GoogleLogin} from 'react-google-login'
 import {Image} from "react-bootstrap"
 
 import NETWORK from '../network'
 import labLogoSrc from "../assets/lab_logo.png"
 import gLogoSrc from "../assets/g_normal.png"
+import {LabLoader} from "../components/loader"
 import {Footer} from '../components/footer'
 
 import './login_view.scss'
@@ -16,6 +17,8 @@ interface RunsListProps {
 
 function LoginView(props: RunsListProps) {
     const clientID: any = process.env.REACT_APP_GOOGLE_CLIENT_ID
+
+    const [isClicked, setIsClicked] = useState(false)
 
     const params = new URLSearchParams(props.location.search)
 
@@ -29,26 +32,37 @@ function LoginView(props: RunsListProps) {
         })
     }
 
-    return <div className={"login-view"}>
-        <div className={"login-view-centre"}>
-            <Image src={labLogoSrc} thumbnail/>
-            <h1 className={"mt-3"}>LabML</h1>
-            <div className={"mt-5 mb-3"}>
-                <GoogleLogin
-                    clientId={clientID}
-                    render={renderProps => (
-                        <a onClick={renderProps.onClick} className="customBtn" type='button'>
-                            <Image className={"icon"} src={gLogoSrc}/>
-                            <span className="buttonText ">Sign in with Google</span>
-                        </a>
-                    )}
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
+    function onRequest() {
+        setIsClicked(true)
+    }
+
+    return <div>
+        {isClicked ?
+            <LabLoader isLoading={isClicked}/>
+            :
+            <div className={"login-view"}>
+                <div className={"login-view-centre"}>
+                    <Image src={labLogoSrc} thumbnail/>
+                    <h1 className={"mt-3"}>LabML</h1>
+                    <div className={"mt-5 mb-3"}>
+                        <GoogleLogin
+                            clientId={clientID}
+                            render={renderProps => (
+                                <a onClick={renderProps.onClick} className="customBtn" type='button'>
+                                    <Image className={"icon"} src={gLogoSrc}/>
+                                    <span className="buttonText ">Sign in with Google</span>
+                                </a>
+                            )}
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            onRequest={onRequest}
+                            cookiePolicy={'single_host_origin'}
+                        />
+                    </div>
+                </div>
+                <Footer/>
             </div>
-        </div>
-        <Footer/>
+        }
     </div>
 }
 
