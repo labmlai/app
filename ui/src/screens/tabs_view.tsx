ba13ffa2b84c2ff01c1f78ba40d1c3758f5a6956
hnipun <hnipun@gmail.com>
2020-10-05 16:29:20 +0530

    user tab icon

--- ui/src/screens/tabs_view.tsx
+++ ui/src/screens/tabs_view.tsx
@@ -4,7 +4,7 @@ import RunsListView from "./runs_list_view";
 import SettingsView from "./settings_view";
 import {useLocation} from "react-router-dom"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome, faSlidersH} from "@fortawesome/free-solid-svg-icons";
+import {faHome, faUserCircle} from "@fortawesome/free-solid-svg-icons";
 
 
 import './tabs_view.scss'
@@ -16,11 +16,11 @@ function TabsView() {
     return <div>
         <Tab.Container defaultActiveKey="second">
             <div className={'flex-container'}>
-                <Nav.Link eventKey="first" className={'tab'}>
-                    <FontAwesomeIcon icon={faSlidersH}/>
-                </Nav.Link>
                 <Nav.Link eventKey="second"  className={'tab'}>
                     <FontAwesomeIcon icon={faHome}/>
+                </Nav.Link>
+                <Nav.Link eventKey="first" className={'tab'}>
+                    <FontAwesomeIcon icon={faUserCircle}/>
                 </Nav.Link>
             </div>
             <Tab.Content>
