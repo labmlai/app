055cfb87a720315e9daa3668df43f4d001cb0095
hnipun <hnipun@gmail.com>
2020-10-12 08:27:21 +0530

    changed loader Bars -> Oval

--- ui/src/components/loader.tsx
+++ ui/src/components/loader.tsx
@@ -9,10 +9,10 @@ interface LabLoaderProps {
 export function LabLoader(props: LabLoaderProps) {
     return <div className={'loader'}>
         <Loader
-            type="Bars"
+            type="Oval"
             color="#eb6134"
-            height={75}
-            width={75}
+            height={50}
+            width={50}
             visible={props.isLoading}
         />
     </div>
