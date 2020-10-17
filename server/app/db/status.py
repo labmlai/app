c4d9638cd47436ac95f563909f038e4af768230f
hnipun <hnipun@gmail.com>
2020-10-17 12:25:07 +0530

    status

--- server/app/db/status.py
+++ server/app/db/status.py
@@ -1,33 +1,32 @@
 import time
 
-from labml_db import Model, Index
+from labml_db import Model, Index, Key
 
 from ..enums import Enums
 
 
-class RunStatusModel:
+class RunStatus(Model['RunStatusModel']):
     status: str
     details: object
     time: float
 
-    def __init__(self, status, details, time):
-        self.status = status
-        self.details = details
-        self.time = time
+    @classmethod
+    def defaults(cls):
+        return dict(status=Enums.RUN_IN_PROGRESS,
+                    details=None,
+                    time=None)
 
 
 class Status(Model['Status']):
     run_uuid: str
     last_updated_time: float
-    run_status: RunStatusModel
+    run_status: Key[RunStatus]
 
     @classmethod
     def defaults(cls):
-        time_now = time.time()
-
         return dict(run_uuid='',
-                    last_updated_time=time_now,
-                    run_status=RunStatusModel(status=Enums.RUN_IN_PROGRESS, details=None, time=time_now)
+                    last_updated_time=None,
+                    run_status=None
                     )
 
 
@@ -39,8 +38,13 @@ def get_or_create(run_uuid: str) -> Status:
     status_key = StatusIndex.get(run_uuid)
 
     if not status_key:
-        status = Status(run_uuid=run_uuid)
+        time_now = time.time()
+
+        run_status = RunStatus(time=time_now)
+        status = Status(run_uuid=run_uuid, last_updated_time=time_now, run_status=run_status)
         status.save()
+        run_status.save()
+
         StatusIndex.set(run_uuid, status.key)
 
         return status
