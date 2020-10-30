6e8d38693cdc5c3c6e0eff8a07279ea2b21b20d8
hnipun <hnipun@gmail.com>
2020-10-30 13:47:48 +0530

    not less than

--- ui/src/components/utils.tsx
+++ ui/src/components/utils.tsx
@@ -10,7 +10,7 @@ export function getTimeDiff(timestamp: number): string {
     let timeDiff = (Date.now() / 1000 - timestamp / 1000)
 
     if (timeDiff < 60) {
-        return `less than ${Math.round(timeDiff)} secs ago`
+        return `${Math.round(timeDiff)} secs ago`
     } else if (timeDiff < 600) {
         return `${Math.floor(timeDiff / 60)} minutes and ${Math.round(timeDiff % 60)} secs ago`
     } else {
