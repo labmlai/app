f63860d62a8b00fbf0a061ec37ea809286a1a55e
hnipun <hnipun@gmail.com>
2020-10-30 10:17:18 +0530

    updated: sample code

--- ui/src/components/code.tsx
+++ ui/src/components/code.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {LabLoader} from "./loader"
 import {Footer} from './footer'
 
+import "./code.scss"
 
 interface CodeProps {
     labMlToken: string | null
@@ -24,41 +25,50 @@ export function Code(props: CodeProps) {
 
             <h6>or</h6>
             <h6 className={'text-dark font-weight-bolder'}>Start monitoring your models by adding just two lines of
-                code, here is an
-                example</h6>
+                code, here is an example</h6>
         </div>
-        <div className={'mt-3 bg-light container-sm mb-3'}>
-            <code className={"text-secondary"}>
+        <div className={'mt-3 bg-dark container-sm mb-3 pt-1 pb-1'}>
+            <code className={"text-white"}>
                 <p>
-                    from numpy.random import random<br/>
-                    from labml import tracker, experiment
+                    <span className={"key-word"}>from</span> numpy.random
+                    <span className={"key-word"}> import</span> random <br/>
+                    <span className={"key-word"}>from</span> labml <span className={"key-word"}>import</span> tracker,
+                    experiment
                 </p>
                 <p className={'mt-4'}>
-                    conf = {"{'batch_size': 20}"}<br/>
+                    conf = {"{"}<span className={"string"}>'batch_size'</span>: <span className={"value"}>20</span>{"}"}<br/>
                 </p>
                 <p className={'mt-4'}>
                     <span>
-                        def train(n: int):<br/>
+                        <span className={"key-word"}>def</span>  <span className={"method"}>train</span>(n:
+                        <span className={"built-ins"}>int</span>):<br/>
                      </span>
                     <span>
-                        <Tab/>return 0.999 ** n + random() / 10, 1 - .999 ** n +\<br/>
-                        <Tab/><Tab/><Tab/>random() / 10
+                        <Tab/>lss = <span className={"value"}>0.999</span> ** n + random() /
+                        <span className={"value"}>10</span><br/>
+                        <Tab/>acc = <span className={"value"}>1</span> - <span className={"value"}>0.999</span> ** n + random() /
+                         <span className={"value"}>10</span><br/>
+                        <br/>
+                        <Tab/><span className={"key-word"}>return</span> lss, acc
                     </span>
                 </p>
                 <p className={'mt-4'}>
-                    <span className={'font-weight-bolder text-dark'}>
-                        with experiment.record(name='sample', exp_conf=conf, <br/>
-                        <Tab/><Tab/><Tab/>token='{props.labMlToken}'):<br/>
+                    <span className={'bold'}>
+                        <span className={"key-word"}>with</span> experiment.record(
+                        <span className={"param"}>name</span>=<span className={"string"}>'sample'</span>,
+                        <span className={"param"}> exp_conf</span>=conf):<br/>
                     </span>
                     <span>
-                        <Tab/>for i in range(100000):<br/>
+                        <Tab/><span className={"key-word"}>for</span> i
+                        <span className={"key-word"}> in</span> <span className={"built-ins"}>range</span>(
+                        <span className={"value"}>100000</span>):<br/>
                     </span>
                     <span>
                        <Tab/><Tab/>loss, accuracy = train(i)<br/>
                     </span>
-                    <span className={'font-weight-bolder text-dark'}>
-                        <Tab/><Tab/>tracker.save(i, {"{'loss': loss, 'accuracy': "}<br/>
-                        <Tab/><Tab/><Tab/>{"accuracy}"})<br/>
+                    <span className={'bold'}>
+                        <Tab/><Tab/>tracker.save(i, <span className={"param"}>loss</span>=loss,
+                        <span className={"param"}> accuracy</span>=accuracy)
                     </span>
                 </p>
             </code>
