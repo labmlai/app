1f0205a8d9d1d0170aac3f9ec6fe5145cb2a5b24
hnipun <hnipun@gmail.com>
2020-10-31 10:22:48 +0530

    tabs without react-bootstrap

--- ui/src/screens/tabs_view.tsx
+++ ui/src/screens/tabs_view.tsx
@@ -1,40 +1,40 @@
-import React from "react"
-import {Tab, Nav} from "react-bootstrap"
-import RunsListView from "./runs_list_view";
-import SettingsView from "./settings_view";
+import React, {useState} from "react"
+import {Nav} from "react-bootstrap"
+import RunsListView from "../screens/runs_list_view";
+import SettingsView from "../screens/settings_view";
 import {useLocation} from "react-router-dom"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHome, faUserCircle} from "@fortawesome/free-solid-svg-icons";
 
-
 import './tabs_view.scss'
-
 
 function TabsView() {
     const location = useLocation()
+    const [isRunsList, setIsRunsList] = useState(true)
+
+
+    function clickHandle(e: any, tab: string) {
+        if (tab === 'user') {
+            setIsRunsList(false)
+        } else {
+            setIsRunsList(true)
+        }
+    }
 
     return <div>
-        <Tab.Container defaultActiveKey="second">
-            <div className={'flex-container tab-view'}>
-                <Nav.Link eventKey="second"  className={'tab'}>
-                    <FontAwesomeIcon icon={faHome}/>
-                    <span>Home</span>
-                </Nav.Link>
-                <Nav.Link eventKey="first" className={'tab'}>
-                    <FontAwesomeIcon icon={faUserCircle}/>
-                    <span>User</span>
-                </Nav.Link>
-            </div>
-            <Tab.Content>
-                <Tab.Pane eventKey="first">
-                    <SettingsView/>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                    <RunsListView location={location}/>
-                </Tab.Pane>
-            </Tab.Content>
-        </Tab.Container>
+        <div className={'flex-container tab-view'}>
+            <Nav.Link eventKey="second" className={'tab'} onClick={(e: any) => clickHandle(e, 'run_list')}>
+                <FontAwesomeIcon icon={faHome}/>
+                <span>Home</span>
+            </Nav.Link>
+            <Nav.Link eventKey="first" className={'tab'} onClick={(e: any) => clickHandle(e, 'user')}>
+                <FontAwesomeIcon icon={faUserCircle}/>
+                <span>User</span>
+            </Nav.Link>
+        </div>
+        {isRunsList ? <RunsListView location={location}/> : <SettingsView/>}
     </div>
+
 }
 
 export default TabsView
