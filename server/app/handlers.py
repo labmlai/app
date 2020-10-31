cfd7cc08dd2cdebeb39bd586af4ec7a7c12ffe93
hnipun <hnipun@gmail.com>
2020-10-31 09:31:30 +0530

    show only run_uuid is not null

--- server/app/handlers.py
+++ server/app/handlers.py
@@ -172,7 +172,8 @@ def get_runs(labml_token: str) -> flask.Response:
     res = []
     for r in runs_list:
         s = status.get_status(r.run_uuid)
-        res.append({**r.get_data(), **s.get_data()})
+        if r.run_uuid:
+            res.append({**r.get_data(), **s.get_data()})
 
     res = sorted(res, key=lambda i: i['start_time'], reverse=True)
 
