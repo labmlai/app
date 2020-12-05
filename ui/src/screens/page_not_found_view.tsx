f532b4b4e6b7b1776d5309149dbfbd2412e027a5
hnipun <hnipun@gmail.com>
2020-12-05 08:50:00 +0530

    use lottie-web instead of lottie-react-web

--- ui/src/screens/page_not_found_view.tsx
+++ ui/src/screens/page_not_found_view.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 import {useHistory} from "react-router-dom"
 
 import mixpanel from "mixpanel-browser"
-import Lottie from 'lottie-react-web'
+import lottie from 'lottie-web'
 import animation from '../assets/lottie/Chemical.json'
 import {Button} from "react-bootstrap"
 
 import useWindowDimensions from "../utils/window_dimensions"
-
 
 function PageNotFound() {
     mixpanel.track('404')
@@ -19,11 +18,23 @@ function PageNotFound() {
         history.push('/home')
     }
 
+    const lottieElement = useRef(null) as any
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            container: lottieElement.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: animation
+        })
+    }, [lottieElement])
+
     return <div className={'container'}>
         <div className={'text-center mt-1'}>
             <h1 className={'text-danger display-4'}>ERROR #404</h1>
             <div style={{width: Math.min(windowWidth, 500), margin: '0 auto'}}>
-                <Lottie options={{animationData: animation}}/>
+                <div style={{width: 400, margin: '0 auto'}} ref={lottieElement}/>
             </div>
             <h3 className={'text-secondary'}>OOPS! - Could not Find it</h3>
             <Button className={'mt-2'} onClick={onButtonClick}>Go Back to Home</Button>
