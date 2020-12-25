f5960fff9354ad7eeb366076b4067fea032c8e05
hnipun <hnipun@gmail.com>
2020-12-25 19:25:50 +0530

    insights list

---
+++ ui/src/components/insights/insights_list.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+import {InsightModel} from "../../models/insight"
+
+import "./insights_list.scss"
+
+function Insight(props: InsightModel) {
+    let className = 'insight-container'
+    if (props.type === 'danger') {
+        className += ' danger'
+    } else if (props.type === 'warning') {
+        className += ' warning'
+    } else {
+        className += ' success'
+    }
+
+    return <div className={className}>
+        <span>{props.insight}</span>
+        <span className="time-left">{props.time}</span>
+    </div>
+}
+
+interface InsightsListProps {
+    insightList: InsightModel[]
+}
+
+function InsightsList(props: InsightsListProps) {
+    return <div>
+        {props.insightList.map((insight, idx) => (
+            <Insight key={idx}
+                     insight={insight.insight}
+                     type={insight.type}
+                     time={insight.time}/>
+        ))}
+    </div>
+}
+
+export default InsightsList
---
+++ ui/src/models/insight.ts
@@ -0,0 +1,5 @@
+export interface InsightModel {
+    insight: string
+    type: string
+    time : string
+}
