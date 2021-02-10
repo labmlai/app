123909909b5e2c6662d866fd5400ba2f646ba0e5
hnipun <hnipun@gmail.com>
2021-02-10 20:06:37 +0530

    init cache and models

---
+++ ui/src/cache/cache.ts
@@ -0,0 +1,94 @@
+const RELOAD_TIMEOUT = 60 * 1000
+
+function isReloadTimeout(lastUpdated: number): boolean {
+    return (new Date()).getTime() - lastUpdated > RELOAD_TIMEOUT
+}
+
+class BroadcastPromise<T> {
+    // Registers a bunch of promises and broadcast to all of them
+    private isLoading: boolean
+    private resolvers: any[]
+    private rejectors: any[]
+
+    constructor() {
+        this.isLoading = false
+        this.resolvers = []
+        this.rejectors = []
+    }
+
+    private add(resolve: (value: T) => void, reject: (err: any) => void) {
+        this.resolvers.push(resolve)
+        this.rejectors.push(reject)
+    }
+
+    create(load: () => Promise<T>): Promise<T> {
+        let promise = new Promise<T>((resolve, reject) => {
+            this.add(resolve, reject)
+        })
+
+        if (!this.isLoading) {
+            this.isLoading = true
+            // Load again only if not currently loading;
+            // Otherwise resolve/reject will be called when the current loading completes.
+            load().then((res: T) => {
+                this.resolve(res)
+            }).catch((err) => {
+                this.reject(err)
+            })
+        }
+
+        return promise
+    }
+
+    private resolve(value: T) {
+        this.isLoading = false
+        let resolvers = this.resolvers
+        this.resolvers = []
+        this.rejectors = []
+
+        for (let r of resolvers) {
+            r(value)
+        }
+    }
+
+    private reject(err: any) {
+        this.isLoading = false
+        let rejectors = this.rejectors
+        this.resolvers = []
+        this.rejectors = []
+
+        for (let r of rejectors) {
+            r(err)
+        }
+    }
+}
+
+abstract class CacheObject<T> {
+    protected data!: T | any
+    protected broadcastPromise = new BroadcastPromise<T>()
+    private lastUsed: number
+    public lastUpdated: number
+
+    constructor() {
+        this.lastUsed = 0
+        this.lastUpdated = 0
+    }
+
+    abstract async load(...args: any[]): Promise<T>
+
+    async get(isRefresh = false, ...args: any[]): Promise<T> {
+        if (this.data == null || isRefresh) {
+            this.data = await this.load()
+            this.lastUpdated = (new Date()).getTime()
+        }
+
+        this.lastUsed = new Date().getTime()
+
+        return this.data
+    }
+
+    invalidate_cache(): void {
+        this.data = null
+    }
+}
+
