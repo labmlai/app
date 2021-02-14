7bfabe573333b5df297836878c1fd96c39d75db6
hnipun <hnipun@gmail.com>
2021-02-14 17:23:05 +0530

    labels

---
+++ ui/src/components/charts/labels.ts
@@ -0,0 +1,29 @@
+import {WeyaElementFunction} from "../../../../lib/weya/weya"
+import {getColor} from "./constants"
+
+interface LabelsOptions {
+    labels: string[]
+    colorBias?: number
+}
+
+export class Labels {
+    labels: string[]
+    colorBias?: number
+
+    constructor(opt: LabelsOptions) {
+        this.labels = opt.labels
+        this.colorBias = opt.colorBias
+    }
+
+    render($: WeyaElementFunction) {
+        $('div.text-center.labels.text-secondary',
+            $ => {
+                this.labels.map((label, i) => {
+                    const colorBias = this.colorBias ? this.colorBias : 0
+                    $('span', $ => {
+                        $('div.box', {style: {backgroundColor: getColor(i + colorBias)}}, label)
+                    })
+                })
+            })
+    }
+}
