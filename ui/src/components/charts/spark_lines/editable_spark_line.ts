aaf7305e9920392cc863b4f09fd652d829483c4c
hnipun <hnipun@gmail.com>
2021-03-18 16:29:59 +0530

    spark-line width fix

--- ui/src/components/charts/spark_lines/editable_spark_line.ts
+++ ui/src/components/charts/spark_lines/editable_spark_line.ts
@@ -47,7 +47,7 @@ export class EditableSparkLine {
         this.onClick = opt.onClick
         this.isMouseMoveOpt = opt.isMouseMoveOpt
         this.color = this.selected >= 0 ? opt.color : BASE_COLOR
-        this.titleWidth = Math.min(150, Math.round(opt.width * .375))
+        this.titleWidth = Math.min(150, Math.round(opt.width * .35))
         this.chartWidth = opt.width - this.titleWidth * 2
         this.minLastValue = opt.minLastValue
         this.maxLastValue = opt.maxLastValue
