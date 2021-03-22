8b5ed0e9b892a0f551239ad7d7ca8cf8ca84da12
Adithya Narasinghe <adithyasnarasinghe@gmail.com>
2021-03-23 00:06:18 +0530

    New loader for list views

--- ui/src/views/computers_list_view.ts
+++ ui/src/views/computers_list_view.ts
@@ -1,46 +1,47 @@
 import {ROUTER, SCREEN} from '../app'
 import {Weya as $, WeyaElement} from '../../../lib/weya/weya'
 import {ScreenView} from "../screen"
-import {Loader} from "../components/loader"
+import {DataLoader} from "../components/loader"
 import CACHE, {ComputersListCache} from "../cache/cache"
 import {SearchView} from '../components/search';
-import {CancelButton, DeleteButton, EditButton, RefreshButton} from '../components/buttons'
+import {CancelButton, DeleteButton, EditButton} from '../components/buttons'
 import {ComputerListItemModel} from '../models/computer_list'
 import {ComputersListItemView} from '../components/computers_list_item'
 import {HamburgerMenuView} from '../components/hamburger_menu'
 import mix_panel from "../mix_panel"
-import {handleNetworkError} from '../utils/redirect'
 import EmptyComputersList from "./empty_computers_list"
 import {AlertMessage} from "../components/alert"
-
+import {AwesomeRefreshButton} from '../components/refresh_button'
 
 class ComputersListView extends ScreenView {
     computerListCache: ComputersListCache
     currentComputersList: ComputerListItemModel[]
-    elem: WeyaElement
+    elem: HTMLDivElement
     computersListContainer: HTMLDivElement
-    btnContainer: HTMLDivElement
-    loader: Loader
     searchQuery: string
     buttonContainer: HTMLDivElement
     alertContainer: HTMLDivElement
     deleteButton: DeleteButton
     editButton: EditButton
-    refreshButton: RefreshButton
     cancelButton: CancelButton
     isEditMode: boolean
     computersDeleteSet: Set<string>
+    private loader: DataLoader
+    private refresh: AwesomeRefreshButton
 
     constructor() {
         super()
 
         this.computerListCache = CACHE.getComputersList()
 
-        this.loader = new Loader(true)
         this.deleteButton = new DeleteButton({onButtonClick: this.onDelete, parent: this.constructor.name})
         this.editButton = new EditButton({onButtonClick: this.onEdit, parent: this.constructor.name})
-        this.refreshButton = new RefreshButton({onButtonClick: this.onRefresh, parent: this.constructor.name})
         this.cancelButton = new CancelButton({onButtonClick: this.onCancel, parent: this.constructor.name})
+
+        this.loader = new DataLoader(async (force) => {
+            this.currentComputersList = (await this.computerListCache.get(force)).computers
+        })
+        this.refresh = new AwesomeRefreshButton(this.onRefresh.bind(this))
 
         this.searchQuery = ''
         this.isEditMode = false
@@ -49,39 +50,64 @@ class ComputersListView extends ScreenView {
         mix_panel.track('Computers List View')
     }
 
-    render() {
-        this.elem = $('div', $ => {
-            this.alertContainer = $('div')
-            new HamburgerMenuView({
-                title: 'Computers',
-                setButtonContainer: container => this.buttonContainer = container
-            }).render($)
+    async _render() {
+        this.elem.innerHTML = ''
+        $(this.elem, $ => {
+            $('div', $ => {
+                this.alertContainer = $('div')
+                new HamburgerMenuView({
+                    title: 'Computers',
+                    setButtonContainer: container => this.buttonContainer = container
+                }).render($)
 
-            $('div', '.runs-list', $ => {
-                new SearchView({onSearch: this.onSearch}).render($)
-                this.computersListContainer = $('div', '.list.runs-list.list-group', '')
+                $('div', '.runs-list', $ => {
+                    new SearchView({onSearch: this.onSearch}).render($)
+                    this.loader.render($)
+                    this.computersListContainer = $('div', '.list.runs-list.list-group', '')
+                })
             })
-            this.loader.render($)
+        })
+        $(this.buttonContainer, $ => {
+            this.deleteButton.render($)
+            this.cancelButton.render($)
+            this.editButton.render($)
+            this.refresh.render($)
+            this.deleteButton.hide(true)
+            this.cancelButton.hide(true)
+            this.editButton.hide(true)
         })
 
-        this.renderList().then()
+        try {
+            await this.loader.load()
+
+            this.renderList().then()
+        } catch (e) {
+
+        }
+    }
+
+    render(): WeyaElement {
+        this.elem = $('div')
+
+        this._render().then()
 
         return this.elem
     }
 
-    renderButtons() {
-        this.buttonContainer.innerHTML = ''
-        $(this.buttonContainer, $ => {
-            if (this.currentComputersList.length) {
-                if (this.isEditMode) {
-                    this.deleteButton.render($)
-                    this.cancelButton.render($)
-                } else {
-                    this.editButton.render($)
-                    this.refreshButton.render($)
-                }
-            }
-        })
+    destroy() {
+        this.refresh.stop()
+    }
+
+    updateButtons() {
+        let noRuns = this.currentComputersList.length == 0
+        this.deleteButton.hide(noRuns || !this.isEditMode)
+        this.cancelButton.hide(noRuns || !this.isEditMode)
+        this.editButton.hide(noRuns || this.isEditMode)
+        if (!noRuns && !this.isEditMode) {
+            this.refresh.start()
+        } else {
+            this.refresh.stop()
+        }
     }
 
     renderAlertMessage() {
@@ -99,20 +125,22 @@ class ComputersListView extends ScreenView {
     }
 
     onRefresh = async () => {
+        this.editButton.disabled = true
         try {
-            this.currentComputersList = (await this.computerListCache.get(true)).computers
+            await this.loader.load(true)
+
+            await this.renderList()
         } catch (e) {
-            //TODO: redirect after multiple refresh failures
-            handleNetworkError(e)
-            return
+
+        } finally {
+            this.editButton.disabled = false
         }
-        await this.renderList()
     }
 
     onEdit = () => {
         this.isEditMode = true
         this.deleteButton.disabled = this.computersDeleteSet.size === 0
-        this.renderButtons()
+        this.updateButtons()
     }
 
     onDelete = async () => {
@@ -120,6 +148,7 @@ class ComputersListView extends ScreenView {
             this.renderAlertMessage()
         })
 
+        this.isEditMode = false
         this.computersDeleteSet.clear()
         this.deleteButton.disabled = this.computersDeleteSet.size === 0
 
@@ -155,20 +184,9 @@ class ComputersListView extends ScreenView {
     }
 
     private async renderList() {
-        try {
-            this.currentComputersList = (await this.computerListCache.get()).computers
-        } catch (e) {
-            handleNetworkError(e)
-            return
-        }
-
-        this.loader.remove()
-
         if (this.currentComputersList.length > 0) {
             let re = new RegExp(this.searchQuery.toLowerCase(), 'g')
             this.currentComputersList = this.currentComputersList.filter(computer => this.computersFilter(computer, re))
-
-            this.renderButtons()
 
             this.computersListContainer.innerHTML = ''
             $(this.computersListContainer, $ => {
@@ -184,6 +202,7 @@ class ComputersListView extends ScreenView {
                 new EmptyComputersList().render($)
             })
         }
+        this.updateButtons()
     }
 
 }
