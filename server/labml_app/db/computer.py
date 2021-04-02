4366e44bc9311bda9ed90eebb5730e808e7b5f6d
hnipun <hnipun@gmail.com>
2021-04-02 09:54:17 +0530

    fixed bug in ComputerIndex

--- server/labml_app/db/computer.py
+++ server/labml_app/db/computer.py
@@ -34,7 +34,7 @@ def get_or_create(computer_uuid: str) -> Computer:
         computer = Computer(computer_uuid=computer_uuid,
                             )
         computer.save()
-        ComputerIndex.set(computer_uuid, computer_key)
+        ComputerIndex.set(computer_uuid, computer.key)
 
         return computer
 
