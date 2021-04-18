fcfa63b18751e5731cea1a9fb351dbf4bc48ff6b
hnipun <hnipun@gmail.com>
2021-04-18 13:19:24 +0530

    Series

--- server/labml_app/unit_tests/series.py
+++ server/labml_app/unit_tests/series.py
@@ -1,59 +1,84 @@
 from numpy.random import random, randint
 
+from labml_app.db import analyses
 
-# from labml_app import analyses
 
 def update_equal_gap_equal_sizes(prev: int = 0, size=5, gap: int = 1, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step:
         last = prev + size
 
         step = [*range(prev, last, gap)]
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = step[-1] + gap
 
-        # data = {'step': step, 'value': value}
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
 
 
 def update_equal_gap_equal_sizes_diff_gap_between(prev: int = 0, size=5, gap: int = 1, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step:
         last = prev + size
 
         step = [*range(prev, last, gap)]
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = last
 
-        # data = {'step': step, 'value': value}
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
 
 
 def update_equal_gap_diff_sizes(prev: int = 0, gap: int = 1, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step:
         size = randint(1, 9)
         last = prev + size
 
         step = [*range(prev, last, gap)]
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = step[-1] + gap
 
-        # data = {'step': step, 'value': value}
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
 
 
 def update_equal_gap_diff_sizes_diff_gap_between(prev: int = 0, gap: int = 1, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step:
         size = randint(1, 9)
         last = prev + size
 
         step = [*range(prev, last, gap)]
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = last
 
-        # data = {'step': step, 'value': value}
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
 
 
 def update_diff_gap_diff_sizes(prev: int = 0, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step:
         size = randint(1, 6)
         last = prev + size
@@ -62,21 +87,32 @@ def update_diff_gap_diff_sizes(prev: int = 0, max_step=100):
         for s in range(size):
             last = last + randint(1, 10)
             step.append(last)
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = last
 
-        # data = {'step': step, 'value': value}
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
 
 
 def update_equal_and_diff_gaps_diff_sizes(prev: int = 0, size=5, gap: int = 1, max_step=100):
+    data: analyses.SeriesModel = analyses.series.Series().to_data()
     while prev <= max_step / 2:
         last = prev + size
 
         step = [*range(prev, last, gap)]
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = step[-1] + gap
+
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
 
     while prev <= max_step:
         size = randint(1, 6)
@@ -86,21 +122,27 @@ def update_equal_and_diff_gaps_diff_sizes(prev: int = 0, size=5, gap: int = 1, m
         for s in range(size):
             last = last + randint(1, 10)
             step.append(last)
-        value = random(len(step))
+        value = random(len(step)).tolist()
 
         prev = last
 
+        s = analyses.series.Series().load(data)
+        s.update(step, value)
+
+        data = s.to_data()
+
+    print(data['step'])
+
 
 if __name__ == "__main__":
-    pass
-    # update_equal_gap_equal_sizes()
-    # update_equal_gap_equal_sizes(gap=2)
-    # update_equal_gap_equal_sizes_diff_gap_between(gap=2)
+    # update_equal_gap_equal_sizes(max_step=int(1e+4))
+    # update_equal_gap_equal_sizes(gap=2, max_step=int(1e+4))
+    # update_equal_gap_equal_sizes_diff_gap_between(gap=2, max_step=int(1e+4))
 
-    # update_equal_gap_diff_sizes()
-    # update_equal_gap_diff_sizes(gap=2)
-    # update_equal_gap_diff_sizes_diff_gap_between(gap=2)
+    # update_equal_gap_diff_sizes(max_step=int(1e+4))
+    # update_equal_gap_diff_sizes(gap=2, max_step=int(1e+4))
+    # update_equal_gap_diff_sizes_diff_gap_between(gap=2, max_step=int(1e+4))
 
-    # update_diff_gap_diff_sizes()
+    # update_diff_gap_diff_sizes(max_step=int(1e+4))
 
-    # update_equal_and_diff_gaps_diff_sizes()
+    update_equal_and_diff_gaps_diff_sizes(max_step=int(1e+4))
