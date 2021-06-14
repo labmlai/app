d40ca46dec5faae6e5afc3a312021df08fb922f8
Nipun Wijerathne <hnipun@gmail.com>
2021-06-14 13:11:41 +0530

    Update gunicorn.conf.py

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -12,7 +12,7 @@ accesslog = '../logs/api-access.log'
 
 bind = '0.0.0.0:5000'
 workers = 2  # multiprocessing.cpu_count() * 2 + 1
-threads = 32
+threads = 8
 
 timeout = 3 * 60  # 3 minutes
 keepalive = 24 * 60 * 60  # 1 day
