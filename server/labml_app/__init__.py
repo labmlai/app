15e449d92dc37aea9dca40da90425d86a80fed6e
hnipun <hnipun@gmail.com>
2021-07-09 11:41:47 +0530

    no login requirement for local

--- server/labml_app/__init__.py
+++ server/labml_app/__init__.py
@@ -1,12 +1,18 @@
 import subprocess
 
 
+def install_pipenv_packages():
+    try:
+        subprocess.run(["pipenv install --ignore-pipfile"], shell=True)
+    except KeyboardInterrupt:
+        pass
+
+
 def start_server():
     try:
-        subprocess.run(["pipenv install --ignore-pipfile"], shell=True, capture_output=True)
         subprocess.run(
             ["pipenv run gunicorn --bind 0.0.0.0:5000 -w 2 -k uvicorn.workers.UvicornWorker labml_app.flask_app:app"],
             shell=True,
-            capture_output=True)
+        )
     except KeyboardInterrupt:
         pass
